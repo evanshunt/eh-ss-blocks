e374b22061239ac92884ea7c8cbec7c21ce92e46
Brad Leither <bradl@evanshunt.com>
2018-06-07 14:05:43 -0600

    Target proper image carousel element

--- js/carousel-slick-init.js
+++ js/carousel-slick-init.js
@@ -1,5 +1,5 @@
 jQuery(function ($)
 {
   //IMGCAROUSEL_OPTIONS is defined in Carousel.php by the Carousel::Requirements() method.
-  $('.image-carousel').slick(GLOBAL_IMGCAROUSEL_OPTIONS);
+  $('.image-carousel .carousel-items').slick(GLOBAL_IMGCAROUSEL_OPTIONS);
 });
